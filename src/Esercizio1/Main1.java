22f34965e1c93488bc876f307c8ff0ac192f613b
federicopignatelli <federicopignatelli@icloud.com>
2023-11-28 15:43:21 +0100

    primo esercizio fatto

---
+++ src/Esercizio1/Main1.java
@@ -0,0 +1,20 @@
+package Esercizio1;
+
+public class Main1 {
+
+
+    public static void main(String[] args) {
+        String parola = "ciaone";
+
+        if (stringaPariDispari(parola)){
+            System.out.println("La tua parola ha caratteri pari");
+        }
+        else {
+            System.out.println("La tua parola ha caratteri dispari");
+        }
+    }
+    public static boolean stringaPariDispari (String word){
+        return word.length() % 2 == 0;
+    }
+
+}
